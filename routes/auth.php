e08ab046c1ad1d2babba84cc849bf829dad5f6a5
Kristy Kelly <kristy@kristykelly.com>
2025-11-29 17:04:50 -0500

    Fix duplicate route name 'login' by renaming auth route to 'auth.login'

--- routes/auth.php
+++ routes/auth.php
@@ -14,7 +14,7 @@ Route::post('/register', [RegisteredUserController::class, 'store'])
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest')
-    ->name('login');
+    ->name('auth.login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware('guest')
