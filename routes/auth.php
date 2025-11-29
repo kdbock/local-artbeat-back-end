6b8183bda5b90a6671d128f1e042c05cbcf13ad7
Kristy Kelly <kristy@kristykelly.com>
2025-11-29 16:59:42 -0500

    Fix duplicate route name 'register' by renaming auth route to 'auth.register'

--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
-    ->name('register');
+    ->name('auth.register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware('guest')
